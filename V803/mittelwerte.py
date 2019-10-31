b4360c80ccf7ee9b27c066cf79f86d984d1c80a9
Nico Guth <nico.guth@tu-dortmund.de>
2019-10-31 13:05:52 +0100

    V803 Ausgleichsrechnung Mittelwert

---
+++ V803/mittelwerte.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+x,F = np.genfromtxt('data.csv',delimiter=',',unpack=True)
+
+print("Mittelwerte von")
+Fx = np.mean(F*x)
+print("F*x",Fx)
+Fmean = np.mean(F)
+print("F",Fmean)
+xmean = np.mean(x)
+print("x",xmean)
+x2mean = np.mean(x**2)
+print("x²",x2mean)
+xmean2 = np.mean(x)**2
+print("<x>²",xmean2)
+
+print("D=",(Fx-Fmean*xmean)/(x2mean-xmean2))

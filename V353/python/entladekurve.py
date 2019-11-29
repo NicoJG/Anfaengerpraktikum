2a7e19aa7d681ab5d2f7627a632f3e24dc93f1ab
Nico Guth <nico.guth@tu-dortmund.de>
2019-11-29 13:26:23 +0100

    V353 Entladekurve log und curve-fit

--- V353/python/entladekurve.py
+++ V353/python/entladekurve.py
@@ -1,14 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
+
+def f(x,a,b):
+    return a*x + b
 
 U_C,t = np.genfromtxt('data/entladekurve.csv',delimiter=',',unpack=True)
+t=t*10**(-3)
+U_C = U_C
 
-plt.plot(t,U_C,'ko', label='Abgelesene Punkte')
+U_0 = 1 #Spannung bei t=0
+
+tRange = np.linspace(np.min(t),np.max(t),100)
+params, pcov = curve_fit(f,t,np.log(U_C))
+
+print(params)
+print(params[0])
+print('RC=',1/params[0])
+print('RC(10%)=',1.7*10**-3/2.3)
+
+plt.plot(t*10**3,np.log(U_C/U_0),'kx', label='Gemessen')
+plt.plot(tRange*10**3,f(tRange,*params),'k-', label='Ausgleichsgerade')
 #plt.xlim(np.min(t),np.max(t))
 #plt.ylim(np.min(U_C),np.max(U_C))
-#plt.yscale('log', basey=2.0)
+#plt.yscale('log', basey=np.e)
 plt.xlabel(r'$t \:/\: \si{\milli\second}$')
-plt.ylabel(r'$U_C \:/\: \si{\volt}$')
+plt.ylabel(r'$\ln\left(\frac{U_C}{U_0}\right) \:/\: \si{\volt}$')
 plt.legend(loc='best')
 
 plt.grid(True,which="both", linestyle='--')

d5f0b0e7dd5b6ecad3c604d4fd97130b7af3cf8b
Nico Guth <nico.guth@tu-dortmund.de>
2019-12-07 15:46:34 +0100

    V355 Python Dateien fast gefixt

--- V355/amplitudentheorie.py
+++ V355/amplitudentheorie.py
@@ -3,13 +3,7 @@ import numpy as np
 import json
 from scipy.optimize import curve_fit
 
-def I_Theorie(U,w,Ck,R,L,C):
-    Z = w*L - 1/w * (1/C + 1/Ck)
-    return U/np.sqrt(4*w**2 * Ck**2 * R**2 * Z**2 + (1/(w*Ck) - w*Ck*Z**2 + w* R**2 * Ck)**2)
-
-def func(L,C,c):
-    return 2* np.pi/(2 *np.pi * np.sqrt(L/((1/C)+2/(c))))
-
+# Konstanten
 L = 23.9540 * 10**(-3) #H
 C = 0.8212 * 10**(-9) #F
 R = 48 #Ohm
@@ -17,34 +11,43 @@ U = 10 #V
 UPlus = 8 #V
 UMinus = 7.5 #V
 
-#Ck , vPlus , vMinus = np.genfromtxt('frequenzen.csv', delimiter=",", unpack=True)
-Ck , vMinusgemessen , vMinusTheorie = np.genfromtxt('frequenzentheorie.csv', delimiter=",", unpack=True)
-Ck = Ck* 10**(-9) #F
-#vPlus = vPlus * 10**(3) #Hz
-#vMinus = vMinus * 10**(3) #Hz
-vMinusgemessen = vMinusgemessen * 10**(3) #Hz
-vMinusTheorie = vMinusTheorie * 10**(3) #Hz
+# Theorie Formeln
+def I_Theorie(U0,w,Ck): # I(Ck) alles andere sind konstanten bzw. w wird auch berechnet
+    Z = w*L - 1/w * (1/C + 1/Ck)
+    return U0/np.sqrt(4*w**2 * Ck**2 * R**2 * Z**2 + (1/(w*Ck) - w*Ck*Z**2 + w* R**2 * Ck)**2)
 
-Ck_ , U2Plus, U2Minus, Uk = np.genfromtxt('amplitude.csv', delimiter=",", unpack=True)
-#U2Plus = U2Plus * (1/2)
-#U2Minus = U2Minus * (1/2)
+def wMinus_Theorie(L,C,Ck):
+    return 1/np.sqrt(L/((1/C)+2/(Ck))) #- 1.5*10**(3)
 
-I2Plus = U2Plus / R
-I2Minus = U2Minus / R
-Ik = Uk / R
+def wPlus_Theorie(L,C,Ck):
+    return 1/np.sqrt(L*C)
 
-I1 = U / R
-wMinus = 2 * np.pi * vMinusTheorie
-wPlusTheorie = 35884 * 2 * np.pi
+# Messwerte
+Ck_gemessen , vPlus_gemessen , vMinus_gemessen = np.genfromtxt('frequenzen.csv', delimiter=",", unpack=True)
+Ck_gemessen = Ck_gemessen* 10**(-9) #F
+vPlus_gemessen = vPlus_gemessen * 10**(3) #Hz
+vMinus_gemessen = vMinus_gemessen * 10**(3) #Hz
 
-c= np.linspace(2,12,100)
+Ck_gemessen_2 , U2Plus_gemessen, U2Minus_gemessen, Uk_gemessen = np.genfromtxt('amplitude.csv', delimiter=",", unpack=True)
+Ck_gemessen_2 = Ck_gemessen_2*10**(-9) #F
+I2Plus_gemessen = U2Plus_gemessen / R #A
+I2Minus_gemessen = U2Minus_gemessen / R #A
+Ik_gemessen = Uk_gemessen / R #A
+
+print(I2Plus_gemessen)
+print(I2Minus_gemessen)
 
 
-# Plot der Daten
-plt.plot(Ck_, I2Plus, 'rx', label=r'I2+ \text{gemessen}')
-#plt.plot(Ck_, I2Minus, 'gx', label=r'I2- \text{gemessen}')
-plt.plot(c, I_Theorie(UPlus,wPlusTheorie,c*10**(-9),R,L,C), 'y', label=r'I2+ \text{berechnet}')
-#plt.plot(c, I_Theorie(UMinus,func(L,C,c*10**(-9)),c*10**(-9),R,L,C), 'b', label=r'I2- \text{berechnet}')
+Ck_linspace = np.linspace(1.5,12.5,100)*10**(-9) #F
+
+
+# Plot der Messwerte
+plt.plot(Ck_gemessen_2*10**(9), I2Plus_gemessen, 'rx', label=r'I2+ gemessen')
+plt.plot(Ck_gemessen_2*10**(9), I2Minus_gemessen, 'yx', label=r'I2- gemessen')
+
+# Plot der Theoriekurve
+plt.plot(Ck_linspace*10**(9), I_Theorie(UPlus,wPlus_Theorie(L,C,Ck_linspace),Ck_linspace), 'g-', label=r'I2+ berechnet')
+plt.plot(Ck_linspace*10**(9), I_Theorie(UMinus,wMinus_Theorie(L,C,Ck_linspace),Ck_linspace), 'b-', label=r'I2- berechnet')
 
 # Achsenbeschriftung
 plt.xlabel(r'$C_\text{k} \:/\: \si{\nano\farad}$')
@@ -53,6 +56,7 @@ plt.ylabel(r'$I \:/\: \si{\ampere}$')
 # in matplotlibrc leider (noch) nicht möglich
 plt.legend(loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.grid(True,which="both", linestyle='--')
 
 # Speicherort
 plt.savefig('build/plot_amplitudentheorie.pdf')
--- V355/frequenzentheorie.py
+++ V355/frequenzentheorie.py
@@ -3,30 +3,39 @@ import numpy as np
 import json
 from scipy.optimize import curve_fit
 
-def func(L,C,Ckneu):
-    return 1/(2 *np.pi * np.sqrt(L/((1/C)+2/(Ckneu))))
+def vMinus_theorie(L,C,Ck):
+    return 1/(2 *np.pi * np.sqrt(L/((1/C)+2/(Ck))))
 
+def vPlus_theorie(L,C):
+    return 1/(2*np.pi*np.sqrt(L*C))
 
-# Daten einlesen
-Ck,vg,vg2 = np.genfromtxt('frequenzentheorie.csv',delimiter=',',unpack=True)
-vg = vg*1000
+# Konstanten im Experiment
 L = 23.9540 * 10**(-3)
 C = 0.7932 *10**(-9)
-Ckneu = Ck*10**(-9)
 
-x= np.linspace(0.5,12,1000)
+# Daten einlesen
+Ck_gemessen,vPlus_gemessen,vMinus_gemessen = np.genfromtxt('frequenzen.csv',delimiter=',',unpack=True)
+vPlus_gemessen = vPlus_gemessen*10**3 # Hz
+vMinus_gemessen = vMinus_gemessen*10**3 # Hz
+Ck_gemessen = Ck_gemessen*10**(-9) # F
+
+Ck_linspace= np.linspace(0.5,12.5,100)
 # Plot der Daten
-plt.plot(Ck, vg, 'rx', label=r'v \text{gemessen}')
-plt.plot(x, func(L,C,x*10**(-9)), 'b', label=r'v \text{berechnet}')
+plt.plot(Ck_gemessen*10**(9), vMinus_gemessen*10**(-3), 'rx', label='Messwerte')
+#plt.plot(Ck_gemessen*10**(9), vPlus_gemessen*10**(-3), 'kx', label='Messwerte (v+)')
+# Plot der Theoriekurve
+plt.plot(Ck_linspace, vMinus_theorie(L,C,Ck_linspace*10**(-9))*10**(-3), 'b-', label='Theoriekurve')
+#plt.plot(Ck_linspace, (Ck_linspace/Ck_linspace)*vPlus_theorie(L,C)*10**(-3), 'y-', label='Theoriekurve (v+)')
 
 
 # Achsenbeschriftung
 plt.xlabel(r'$C_\text{k} \:/\: \si{\nano\farad}$')
-plt.ylabel(r'$f \:/\: \si{\hertz}$')
+plt.ylabel(r'$\nu \:/\: \si{\kilo\hertz}$')
 
 # in matplotlibrc leider (noch) nicht möglich
 plt.legend(loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.grid(True,which="both", linestyle='--')
 
 # Speicherort
 plt.savefig('build/plot_frequenzentheorie.pdf')

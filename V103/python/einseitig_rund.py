9a0c5bd4cf3cba85789de91c89a166f4cf0308a0
Daverku <david.venker@tu-dortmund.de>
2019-12-07 15:27:12 +0100

    Plot mit Messwerten und Curve Fit ist fertig

--- V103/python/einseitig_rund.py
+++ V103/python/einseitig_rund.py
@@ -3,9 +3,17 @@ import numpy as np
 import json
 from scipy.optimize import curve_fit
 
+F = 0.7474 * 9.81 #Gewichtskraft
+L = 0.450 #Meter
+I= (np.pi/4) * 0.005**4 #Meter^4
+
 # Funktion für Curve Fit:
-def f(x,F,E,I,L):
-    return F/(2* E * I)* (L*x^2 - (x^3)/3)
+
+def D_Theorie(x,F,E,I,L):
+    return F/(2* E * I)* (L*x**2 - (x**3)/3)
+
+def D_fit(x,E):
+    return D_Theorie(x,F,E,I,L)
 
 # Daten einlesen
 x,D0,DM = np.genfromtxt('data/einseitig_rund.csv',delimiter=',',unpack=True)
@@ -16,33 +24,31 @@ D0 = D0*10**(-3)
 DM = DM*10**(-3)
 D= D0-DM
 
-F = 0.7474 * 9.81 #Gewichtskraft
-L = 0.450 #Meter
-#I=
-
-
 # Ausgleichskurve berechnen
-#params,pcov = curve_fit(f,x,E,I,L)
-#a = params[0]
-#b = params[1]
+params,pcov = curve_fit(D_fit,x,D)
+a = params[0]
 
 #Fehler berechnen
-#a_err = np.absolute(pcov[0][0])**0.5
-#b_err = np.absolute(pcov[1][1])**0.5
+E_err = np.absolute(pcov[0][0])**0.5
+
 
 # Plot der Daten
-plt.plot(x, D, 'rx', label='Auslenkung')
+plt.plot(x*10**(3), D*10**(3), 'rx', label='Auslenkung')
 # Plot der Ausgleichskurve
-#x_linspace = np.linspace(np.min(x),np.max(x),100)
-#plt.plot(x_linspace, f(x_linspace,*params), 'k-', label='Ausgleichskurve')
+x_linspace = np.linspace(np.min(x),np.max(x),100)
+plt.plot(x_linspace*10**(3), D_fit(x_linspace,*params)*10**(3), 'k-', label='Ausgleichskurve')
 
 # Achsenbeschriftung
-plt.xlabel(r'$x \:/\: \si{\meter}$')
-plt.ylabel(r'$D \:/\: \si{\meter}$')
+plt.xlabel(r'$x \:/\: \si{\milli\meter}$')
+plt.ylabel(r'$D \:/\: \si{\milli\meter}$')
 
 # in matplotlibrc leider (noch) nicht möglich
 plt.legend(loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.grid(True,which="both", linestyle='--')
 
 # Speicherort
 plt.savefig('build/plot_einseitig_rund.pdf')
+
+print('E:',a*10**(-9))
+print('Fehler von E',E_err*10**(-9))

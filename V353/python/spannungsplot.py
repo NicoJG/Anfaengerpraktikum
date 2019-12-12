9cd8b3e524a8f7c615a1625ae6dc6bf28b20af74
Nico Guth <nico.guth@tu-dortmund.de>
2019-12-12 21:55:35 +0100

    V353 Auswertung Teil 2.1 korrigiert

--- V353/python/spannungsplot.py
+++ V353/python/spannungsplot.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 from scipy.optimize import curve_fit
+from uncertainties import ufloat
 
-def U(f,RC):
-    return 1/np.sqrt(1+(2*np.pi*f)**2*RC**2)
+def U(f,a):
+    return 1/(np.sqrt(1+(2*np.pi*f)**2*a))
 
 f,U_C,a_g,a = np.genfromtxt('data/gemessen_und_a.csv',delimiter=',',unpack=True)
 
@@ -14,9 +15,16 @@ U_G = 1 #V
 
 params,pcov = curve_fit(U,f,U_C/U_G)
 
+param_a = params[0]
+param_a_err = np.absolute(pcov[0][0])**0.5
+
+RC = ufloat(param_a,param_a_err)**0.5
+
 RC_Ergebnisse = json.load(open('data/RC_Ergebnisse.json','r'))
-RC_Ergebnisse['spannung[s]'] = params[0]
-RC_Ergebnisse['spannung_err[s]'] = np.absolute(pcov[0][0])**0.5
+RC_Ergebnisse['spannung_a[s]'] = param_a
+RC_Ergebnisse['spannung_a_err[s]'] = param_a_err
+RC_Ergebnisse['spannung_RC[s]'] = RC.n
+RC_Ergebnisse['spannung_RC_err[s]'] = RC.s
 json.dump(RC_Ergebnisse, open('data/RC_Ergebnisse.json','w'), indent=4)
 
 f_linspace = np.linspace(np.min(f),np.max(f),1000)

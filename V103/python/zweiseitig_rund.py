b5ec1723769cee119f12b106b7f3a216debd0d40
Nico Guth <nico.guth@tu-dortmund.de>
2020-01-09 16:16:48 +0100

    V103 neue beidseitige Plots

--- V103/python/zweiseitig_rund.py
+++ V103/python/zweiseitig_rund.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import json
 from scipy.optimize import curve_fit
+from uncertainties import ufloat
 
 F = 4.7188 * 9.81 #Gewichtskraft
 L = 0.555 #Meter
@@ -10,11 +11,14 @@ E_vorher = (86.64)*10**9 #Pascal
 
 # Funktion für Curve Fit:
 
-def D_links_fit(x,E):
-    return F/(48* E * I)* (3* L**(2)*x - (4*x**3))
+def D_gerade(x,a,b):
+    return a*x + b
 
-def D_rechts_fit(x,E):
-    return F/(48* E * I)* ((4*x**3) - (12* L * x**2) + (9* L**2 *x) - (L**3))
+# def D_links_fit(x,E):
+#     return F/(48* E * I)* (3* L**(2)*x - (4*x**3))
+
+# def D_rechts_fit(x,E):
+#     return F/(48* E * I)* ((4*x**3) - (12* L * x**2) + (9* L**2 *x) - (L**3))
 
 # def D_Theorie(x,F,E,I,L):
 #     if x <= (L/2):
@@ -54,35 +58,46 @@ D_split = np.split(D,2)
 D_links = D_split[0]
 D_rechts = D_split[1]
 
+# Komische Achsen
+x_links = (3* L**(2)*x_links - (4*x_links**3))
+x_rechts = ((4*x_rechts**3) - (12* L * x_rechts**2) + (9* L**2 *x_rechts) - (L**3))
+
 # Ausgleichskurve berechnen
-params_links,pcov_links = curve_fit(D_links_fit,x_links,D_links,p0=E_vorher)
-E_links = params_links[0]
-params_rechts,pcov_rechts = curve_fit(D_rechts_fit,x_rechts,D_rechts,p0=E_vorher)
-E_rechts = params_rechts[0]
+params_links,pcov_links = curve_fit(D_gerade,x_links,D_links)
+a_links = params_links[0]
+b_links = params_links[1]
+params_rechts,pcov_rechts = curve_fit(D_gerade,x_rechts,D_rechts)
+a_rechts = params_rechts[0]
+b_rechts = params_rechts[1]
 
 #Fehler berechnen
-E_links_err = np.absolute(pcov_links[0][0])**0.5
-E_rechts_err = np.absolute(pcov_rechts[0][0])**0.5
+a_links_err = np.absolute(pcov_links[0][0])**0.5
+b_links_err = np.absolute(pcov_links[1][1])**0.5
+a_rechts_err = np.absolute(pcov_rechts[0][0])**0.5
+b_rechts_err = np.absolute(pcov_rechts[1][1])**0.5
 
 
+#ufloats erstellen
+a_links_ufloat = ufloat(a_links,a_links_err)
+a_rechts_ufloat = ufloat(a_rechts,a_rechts_err)
+
+# E berechnen
+E_links = F/(48* a_links_ufloat * I)
+E_rechts = F/(48* a_rechts_ufloat * I)
+
+###########################
+#linker Plot
 
 # Plot der Daten
-plt.plot(x*10**(3), D*10**(3), 'ro', label='gemessene Auslenkung')
+plt.plot(x_links, D_links*10**(3), 'ro', label='gemessene Auslenkung')
 
 # Plot der Ausgleichskurve
-#x_links_linspace = np.linspace(np.min(x_links),np.max(x_links),100)
-x_links_linspace = np.linspace(0,np.max(x_links),100)
-plt.plot(x_links_linspace*10**(3), D_links_fit(x_links_linspace,*params_links)*10**3, 'g-', label='Ausgleichskurve links')
-
-x_rechts_linspace = np.linspace(np.min(x_rechts),np.max(x_rechts),100)
-plt.plot(x_rechts_linspace*10**(3), D_rechts_fit(x_rechts_linspace,*params_rechts)*10**3, 'b-', label='Ausgleichskurve rechts')
-
-# Theorie Plot mit dem E Wert aus der anderen Messung (keine Ahnung warum das direkt in mm berechnet wird, eigentlich müsste man ja das Ergebnis *10**3 rechnen...)
-#plt.plot(x_linspace*10**(3), D_Theorie_Array(x_linspace,F,E_vorher,I,L)*10**3, 'b-', label='Theorie')
+x_links_linspace = np.linspace(np.min(x_links),np.max(x_links),100)
+plt.plot(x_links_linspace, D_gerade(x_links_linspace,*params_links)*10**3, 'g-', label='Ausgleichsgerade')
 
 
 # Achsenbeschriftung
-plt.xlabel(r'$x \:/\: \si{\milli\meter}$')
+plt.xlabel(r'$(3 L^{2} x - (4 x^3)) \:/\: \si{\meter\cubed}$')
 plt.ylabel(r'$D \:/\: \si{\milli\meter}$')
 
 # in matplotlibrc leider (noch) nicht möglich
@@ -91,10 +106,36 @@ plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.grid(True,which="both", linestyle='--')
 
 # Speicherort
-plt.savefig('build/plot_zweiseitig_rund.pdf')
+plt.savefig('build/plot_zweiseitig_rund_links.pdf')
 
-print('E4 links[GP]:',E_links*10**(-9))
-print('Fehler von E4_links[GP]',E_links_err*10**(-9))
-print('E4 rechts[GP]:',E_rechts*10**(-9))
-print('Fehler von E4_rechts[GP]',E_rechts_err*10**(-9))
-#print('Verschiebung nach rechts[mm]: ',verschiebung*10**(3))
+plt.clf()
+
+###########################
+#rechter Plot
+
+# Plot der Daten
+plt.plot(x_rechts, D_rechts*10**(3), 'ro', label='gemessene Auslenkung')
+
+# Plot der Ausgleichskurve
+x_rechts_linspace = np.linspace(np.min(x_rechts),np.max(x_rechts),100)
+plt.plot(x_rechts_linspace, D_gerade(x_rechts_linspace,*params_rechts)*10**3, 'g-', label='Ausgleichsgerade')
+
+
+# Achsenbeschriftung
+plt.xlabel(r'$((4 x^3) - (12 L x^2) + (9 L^2 x) - (L^3)) \:/\: \si{\meter\cubed}$')
+plt.ylabel(r'$D \:/\: \si{\milli\meter}$')
+
+# in matplotlibrc leider (noch) nicht möglich
+plt.legend(loc='best')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.grid(True,which="both", linestyle='--')
+
+# Speicherort
+plt.savefig('build/plot_zweiseitig_rund_rechts.pdf')
+
+print('a links: ',a_links_ufloat)
+print('b links: ',b_links,'+-',b_links_err)
+print('E4 links[GP]: ',E_links*10**(-9))
+print('a rechts: ',a_rechts_ufloat)
+print('b rechts: ',b_rechts,'+-',b_rechts_err)
+print('E4 rechts[GP]: ',E_rechts*10**(-9))

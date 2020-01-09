a9e01e29a97fbd15c753058a66f15ed0125f4f11
Nico Guth <nico.guth@tu-dortmund.de>
2020-01-09 18:09:17 +0100

    V203 Auswertung Python a und b

--- V203/python/niedrigdruck.py
+++ V203/python/niedrigdruck.py
@@ -5,54 +5,61 @@ from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
 # Funktion für Curve Fit:
+# Formel benutzt: ln(p) = -L/R*1/T + const
 def f(x,a,b):
     return a*x+b
 
 # Daten einlesen
-x,y,z = np.genfromtxt('data/NAME.csv',delimiter=',',unpack=True)
+T,p = np.genfromtxt('data/niedrigdruck.csv',delimiter=',',unpack=True)
 
 #Berechnungen
-x_new = x
-y_new = y
-z_new = x+y
+T = T + 273.15 #Kelvin
+p = p*10**(-3) + 1 #bar
+p = p*10**5 #Pascal
 
-# Daten speichern
-data = list(zip(x_new,y_new,z))
-np.savetxt('data/NAME.csv', data, header='x[Ohm],y[J],z[V]', fmt='%1.1f,%1.3f,%i')
-# Nicht so wie hier die alten Daten überschreiben!
-# fmt = format (Genauigkeit,...)
+#Konstanten
+p0 = p[0]
+R = 8.314 #J / (mol K)
 
-# Ausgleichskurve berechnen
-params,pcov = curve_fit(f,x,y)
-a = params[0]
-b = params[1]
+# Curve Fit
+params,pcov = curve_fit(f,1/T,np.log(p))
+a = ufloat(params[0],np.absolute(pcov[0][0])**0.5)
+b = ufloat(params[1],np.absolute(pcov[1][1])**0.5)
 
-#Fehler berechnen
-a_err = np.absolute(pcov[0][0])**0.5
-b_err = np.absolute(pcov[1][1])**0.5
+# L berechnen
+L = -a*R
 
-# Einzelne Daten Speichern
+# Ergebnisse laden
 Ergebnisse = json.load(open('data/Ergebnisse.json','r'))
-Ergebnisse['a[A]'] = a
-Ergebnisse['a_err[A]'] = a_err
-Ergebnisse['b[V]'] = b
-Ergebnisse['b_err[V]'] = b_err
+
+# Ergebnisse in die Dictonary
+if not 'niedrigdruck' in Ergebnisse:
+    Ergebnisse['niedrigdruck'] = {}
+
+Ergebnisse['niedrigdruck']['a'] = a.n
+Ergebnisse['niedrigdruck']['a_err'] = a.s
+Ergebnisse['niedrigdruck']['b'] = b.n
+Ergebnisse['niedrigdruck']['b_err'] = b.s
+Ergebnisse['niedrigdruck']['L'] = L.n
+Ergebnisse['niedrigdruck']['L_err'] = L.s
+
+# Ergebnisse Speichern
 json.dump(Ergebnisse,open('data/Ergebnisse.json','w'),indent=4)
 
+# Plot der Ausgleichsgerade
+T_linspace = np.linspace(np.min(T),np.max(T),100)
+plt.plot(1/T_linspace, f(1/T_linspace,*params), 'k-', label='Ausgleichsgerade')
 
-# Plot der Ausgleichskurve
-x_linspace = np.linspace(np.min(x),np.max(x),100)
-plt.plot(x_linspace, f(x_linspace,*params), 'k-', label='Ausgleichskurve')
 # Plot der Daten
-plt.plot(x, y, 'ro', label='Kurve')
+plt.plot(1/T, np.log(p), 'ro', label='gemessene Werte')
 
 # Achsenbeschriftung
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+plt.xlabel(r'$\frac{1}{T} \:/\: \si{\per\kelvin}$')
+plt.ylabel(r'$\ln \left( p \right)$')
 
 # in matplotlibrc leider (noch) nicht möglich
 plt.legend()
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 
 # Speicherort
-plt.savefig('build/plot_NAME.pdf')
+plt.savefig('build/plot_niedrigdruck.pdf')

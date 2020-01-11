286a09080e5e33ffcf2f3f29ff0c8bda6fae4ee2
Nico Guth <nico.guth@tu-dortmund.de>
2020-01-11 18:15:47 +0100

    V203 Auswertung python vorerst fertig

--- V203/python/hochdruck.py
+++ V203/python/hochdruck.py
@@ -4,8 +4,26 @@ import json
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
+# Konstanten
+R = 8.314 #J / (mol K)
+A = 0.9 #J m^3 mol^(-2)
+
 def p_fit(T,a,b,c,d):
     return a*T**3 + b*T**2 + c*T + d
+
+def dpdT(T,a,b,c):
+    return 3*a*T**2 + 2*b*T + c
+
+def VD_plus(T,p):
+    return R*T/(2*p) + (R**2*T**2/(4*p**2)-A/p)**0.5
+def VD_minus(T,p):
+    return R*T/(2*p) - (R**2*T**2/(4*p**2)-A/p)**0.5
+
+def L_plus(T,a,b,c,d):
+    return T * VD_plus(T,p_fit(T,a,b,c,d)) * dpdT(T,a,b,c)
+
+def L_minus(T,a,b,c,d):
+    return T * VD_minus(T,p_fit(T,a,b,c,d)) * dpdT(T,a,b,c)
 
 # Daten einlesen
 T,p = np.genfromtxt('data/hochdruck.csv',delimiter=',',unpack=True)
@@ -61,3 +79,38 @@ plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 
 # Speicherort
 plt.savefig('build/plot_hochdruck.pdf')
+
+#####################
+# Verdampfungswärme Plot
+#####################
+
+plt.clf()
+
+plt.plot(T_linspace,L_plus(T_linspace,*params), 'k-', label='bestimmte Kurve')
+
+# Achsenbeschriftung
+plt.xlabel(r'$T \:/\: \si{\kelvin}$')
+plt.ylabel(r'$L \:/\: \si{\joule\per\mol}$')
+
+# in matplotlibrc leider (noch) nicht möglich
+plt.legend()
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+
+# Speicherort
+plt.savefig('build/plot_hochdruck_plus.pdf')
+
+
+plt.clf()
+
+plt.plot(T_linspace,L_minus(T_linspace,*params), 'k-', label='bestimmte Kurve')
+
+# Achsenbeschriftung
+plt.xlabel(r'$T \:/\: \si{\kelvin}$')
+plt.ylabel(r'$L \:/\: \si{\joule\per\mol}$')
+
+# in matplotlibrc leider (noch) nicht möglich
+plt.legend()
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+
+# Speicherort
+plt.savefig('build/plot_hochdruck_minus.pdf')

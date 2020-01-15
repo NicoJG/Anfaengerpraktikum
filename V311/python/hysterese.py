c46968ee5fb6c0312697a5e3ef553ec4c8da35f2
Daverku <david.venker@tu-dortmund.de>
2020-01-15 09:35:07 +0100

    V311 Erstellen der Plots, Berechnungen folgen noch

---
+++ V311/python/hysterese.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+
+# Funktion für Curve Fit:
+def f(x,a,b):
+    return a*x+b
+
+# Daten einlesen
+Ib,B1,B2 = np.genfromtxt('data/hysterese.csv',delimiter=',',unpack=True)
+
+#Berechnungen
+Ib = Ib #Ampere
+B1 = B1 #Millitesla
+B2 = B2 #Millitesla
+
+# Ausgleichskurve berechnen
+#params,pcov = curve_fit(f,x,y)
+#a = params[0]
+#b = params[1]
+
+#Fehler berechnen
+#a_err = np.absolute(pcov[0][0])**0.5
+#b_err = np.absolute(pcov[1][1])**0.5
+
+# Plot der Ausgleichskurve
+#x_linspace = np.linspace(np.min(x),np.max(x),100)
+#plt.plot(x_linspace, f(x_linspace,*params), 'k-', label='Ausgleichskurve')
+# Plot der Daten
+plt.plot(Ib, B1, 'ro', label='Steigendes B-Feld')
+plt.plot(Ib, B2, 'bo', label='Fallendes B-Feld')
+
+# Achsenbeschriftung
+plt.xlabel(r'$I_B \:/\: \si{\ampere}$')
+plt.ylabel(r'$B \:/\: \si{\milli\tesla}$')
+
+# in matplotlibrc leider (noch) nicht möglich
+plt.legend()
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+
+# Speicherort
+plt.savefig('build/plot_hysterese.pdf')
---
+++ V311/python/zink_Ib.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+
+# Funktion für Curve Fit:
+def f(x,a,b):
+    return a*x+b
+
+# Daten einlesen
+Ib,Uh = np.genfromtxt('data/zink_Ib.csv',delimiter=',',unpack=True)
+
+#Berechnungen
+Ib = Ib #Ampere
+Uh = Uh #MilliVolt
+
+
+# Ausgleichskurve berechnen
+params,pcov = curve_fit(f,Ib,Uh)
+a = params[0]
+b = params[1]
+
+#Fehler berechnen
+a_err = np.absolute(pcov[0][0])**0.5
+b_err = np.absolute(pcov[1][1])**0.5
+
+# Plot der Ausgleichskurve
+x_linspace = np.linspace(np.min(Ib),np.max(Ib),100)
+plt.plot(x_linspace, f(x_linspace,*params), 'k-', label='Ausgleichskurve')
+# Plot der Daten
+plt.plot(Ib, Uh, 'ro', label='Hall-Spannung bei Variation von Ib')
+
+# Achsenbeschriftung
+plt.xlabel(r'$I_B \:/\: \si{\ampere}$')
+plt.ylabel(r'$U_H \:/\: \si{\milli\volt}$')
+
+# in matplotlibrc leider (noch) nicht möglich
+plt.legend()
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+
+# Speicherort
+plt.savefig('build/plot_zink_Ib.pdf')

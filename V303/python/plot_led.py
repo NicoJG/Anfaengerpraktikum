9d2bd7f046d4ed42fcbba276f92bc38b1b88a7d6
Nico Guth <nico.guth@tu-dortmund.de>
2020-01-23 21:40:36 +0100

    V303 Versuchter fix

--- V303/python/plot_led.py
+++ V303/python/plot_led.py
@@ -6,16 +6,22 @@ from uncertainties import ufloat
 
 # Funktion für Curve Fit:
 def U_fit(r,a):
-    return a/r**2
+    return a/(r**2)
 
 # Daten einlesen
 r,U = np.genfromtxt('data/led.csv',delimiter=',',unpack=True)
 
 # Einheiten Umrechnen
-r = r * 10**(-3) #m
+r = r * 10**(-2) #m
+r_gut = r[5:]
+U_gut = U[5:]
+
+print(r)
+print(U)
+print(U_gut)
 
 # Curve Fit
-params,pcov = curve_fit(U_fit,r,U)
+params,pcov = curve_fit(U_fit,r_gut,-U_gut)
 a = params[0]
 a_err = np.absolute(pcov[0][0])**0.5
 
@@ -33,13 +39,14 @@ json.dump(Ergebnisse,open('data/Ergebnisse.json','w'),indent=4)
 r_linspace = np.linspace(np.min(r),np.max(r),100)
 
 # Plot der Ausgleichskurve
-#plt.plot(r_linspace*10**3, U_fit(r_linspace*10**3,*params), 'k-', label='Ausgleichskurve')
+#plt.plot(r_linspace, U_fit(r_linspace,*params), 'k-', label='Ausgleichskurve')
 # Plot der Daten
-plt.plot(r*10**3, U, 'ro', label='Messdaten')
+plt.plot(r, -U, 'ro', label='Messdaten')
+plt.plot(r_gut, -U_gut, 'bo', label='Messdaten')
 
 # Achsenbeschriftung
-plt.xlabel(r'$r \:/\: \si{\milli\meter}$')
-plt.ylabel(r'$U_\text{out} \:/\: \si{\volt}$')
+plt.xlabel(r'$r \:/\: \si{\meter}$')
+plt.ylabel(r'$|U_\text{out}| \:/\: \si{\volt}$')
 
 # in matplotlibrc leider (noch) nicht möglich
 plt.legend()

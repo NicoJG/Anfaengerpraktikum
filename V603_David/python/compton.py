1bf015b883ff62268323ab265295cc8856e623d3
Daverku <david.venker@tu-dortmund.de>
2020-05-03 19:24:06 +0200

    V603 von David einfach mal alles pushen

---
+++ V603_David/python/compton.py
@@ -0,0 +1,77 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+
+# Funktion für Curve Fit:
+def f(x,a,b):
+    return a*x+b
+
+def I(N):
+    return N/(1 - ((90*10**-6)*N))
+
+def L(a):
+    return (2*201.4*10**-12)*np.sin(a)
+
+def X(T,a,b):
+    return (T - b)/a   
+# Daten einlesen
+alpha,N0, Nal= np.genfromtxt('data/absorber.csv',delimiter=',',unpack=True)
+
+#Berechnungen
+t = 1 #sekunden
+N02 = N0 * t
+Nal2 = Nal * t
+alpha = np.radians(alpha)
+T = I(Nal2)/I(N02)
+
+I0 = 2731
+I1 = 1180
+I2 = 1024
+
+T1 = I1/I0
+T2 = I2/I0
+
+a1 = ufloat(-15194732415, 239100069)
+b1 = ufloat(1.23,0.01)
+
+d = 201.4 #pikometer 10^-12
+lam = L(alpha)
+
+# Ausgleichskurve berechnen
+params,pcov = curve_fit(f,lam,T,p0=(-1000,0.5))
+a = params[0]
+b = params[1]
+
+#Fehler berechnen
+a_err = np.absolute(pcov[0][0])**0.5
+b_err = np.absolute(pcov[1][1])**0.5
+
+
+# Plot der Ausgleichskurve
+x_linspace = np.linspace(np.min(lam),np.max(lam),100)
+plt.plot(x_linspace, f(x_linspace,*params), 'k-', label='Ausgleichskurve')
+# Plot der Daten
+plt.plot(lam, T, 'ro', label='Messdaten')
+
+# Achsenbeschriftung
+plt.xlabel(r'$\lambda \:/\: \si{\meter}$')
+plt.ylabel(r'$T$')
+
+# in matplotlibrc leider (noch) nicht möglich
+plt.legend()
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+
+#print(a)
+#print(a_err)
+#print(b)
+#print(b_err)
+
+#print(T1)
+#print(T2)
+#print(X(T1,a1,b1))
+#print(X(T2,a1,b1))
+print(X(T2,a1,b1) - X(T1,a1,b1))
+# Speicherort
+plt.savefig('build/plot_compton.pdf')
--- V603_David/python/emissionCu.py
+++ V603_David/python/emissionCu.py
@@ -3,6 +3,7 @@ import numpy as np
 import json
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
+from scipy.signal import argrelextrema
 
 # Funktion für Curve Fit:
 def f(x,a,b):
@@ -10,6 +11,7 @@ def f(x,a,b):
 
 def E(alpha):
     return (h * c)/(2 *d* np.sin(alpha))
+
 
 # Daten einlesen
 theta, N = np.genfromtxt('data/emissionCU.csv',delimiter=',',unpack=True)
@@ -22,6 +24,8 @@ alpha1 = 0.393
 alpha2 = 0.353
 d = 201.4*10**-12
 
+
+
 # Ausgleichskurve berechnen
 #params,pcov = curve_fit(f,x,y)
 #a = params[0]
@@ -31,22 +35,30 @@ d = 201.4*10**-12
 #a_err = np.absolute(pcov[0][0])**0.5
 #b_err = np.absolute(pcov[1][1])**0.5
 
+i_max = argrelextrema(N, np.greater, order=20)[0] # Indices der relativen Maxima von N
+
 # Plot der Ausgleichskurve
 #x_linspace = np.linspace(np.min(x),np.max(x),100)
 #plt.plot(x_linspace, f(x_linspace,*params), 'k-', label='Ausgleichskurve')
 # Plot der Daten
 plt.plot(theta, N, 'ro', label='Emmissionsspektrum')
 
+
 # Achsenbeschriftung
 plt.xlabel(r'$\theta \:/\: \si{\circ}$')
 plt.ylabel(r'$N \:/\: \frac{\text{Imp}}{\si{\second}}$')
-
+plt.axvline(x=20.2, linewidth=1, linestyle='--', color='g', label='K Beta Linie')
+plt.axvline(x=22.5, linewidth=1, linestyle='--', color='b', label='K Alpha Linie')
+plt.annotate('Bremsberg', xy=(theta[i_max[0]],500),xycoords='data',
+                xytext=(-50, 30), textcoords='offset points',
+                arrowprops=dict(arrowstyle="->"))
 # in matplotlibrc leider (noch) nicht möglich
 plt.legend()
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 
-print(E(alpha1))
-print(E(alpha2))
+#print(E(alpha1))
+#print(E(alpha2))
+
 
 # Speicherort
 plt.savefig('build/plot_emissionCu.pdf')

56b506c3c0bcc1eb85a5f0d326080922ca65f93c
Nico Guth <nico.guth@tu-dortmund.de>
2020-05-10 18:51:21 +0200

    V602 Python Absorption fertig

--- V602_Nico/python/absorption.py
+++ V602_Nico/python/absorption.py
@@ -5,6 +5,7 @@ os.environ['MATPLOTLIBRC'] = (pathlib.Path(__file__).absolute().parent.parent.pa
 os.environ['TEXINPUTS'] =  (pathlib.Path(__file__).absolute().parent.parent.parent / 'default').__str__() + ':'
 #######
 import sys
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import json
@@ -17,10 +18,61 @@ theta = np.zeros([6,21])
 N = np.zeros([6,21]) 
 name = ['Zink','Gallium','Brom','Rubidium','Strontium','Zirkonium']
 size = [16,21,16,14,16,16]
+Z = np.array([30,31,35,37,38,40]) # Ordnungszahl
 
 ## Messwerte einlesen
 for i in range(0,6):
     theta[i][0:size[i]],N[i][0:size[i]] = np.genfromtxt('data/'+name[i]+'.dat',delimiter=',',unpack=True)
+
+# Naturkonstanten
+h = 4.136*10**(-15) # eV s
+c = 2.998*10**(8) # m/s
+R_inf = 13.6 # eV Rydbergkonstante
+alpha = 7.297*10**(-3) # Sommerfeldsche Feinstrukturkonstante
+
+# Messkonstanten
+d = 201.4 * 10**(-12) # m Gitterkonstante LiF
+n = 1 # Beugungsordnung
+
+## Absorptionskante bestimmen
+# Indizes der Mitten der Kanten (abgelesen/abgeschätzt)
+# .5 meint zwischen 2 Messwerten
+i_K = [7, 3.5, 4.5, 6, 6, 5]
+# K-Kante als Winkel
+theta_K = np.zeros(6)
+for i in range(0,6):
+    if i_K[i] % 1 == 0:
+        theta_K[i] = theta[i][i_K[i]]
+    else:
+        # die mitte zwischen den Punkten um den Index herum
+        theta_K[i] = 0.5*(theta[i][math.ceil(i_K[i])] + theta[i][math.floor(i_K[i])])
+
+# K-Kante als Wellenlänge
+l_K = 2*d/n * np.sin(np.deg2rad(theta_K)) # m Wellenlänge
+
+# K-Kante als Energie
+E_K = h*c/l_K # eV Photonenenergie
+
+# Abschirmkonstante
+sigma_K = Z - np.sqrt(E_K/R_inf - alpha**2*Z**4/4)
+
+###############################
+## Ergebnisse speichern JSON
+###############################
+Ergebnisse = json.load(open('data/Ergebnisse.json','r'))
+if not 'Absorption' in Ergebnisse:
+    Ergebnisse['Absorption'] = {}
+
+for i in range(0,6):
+    if not name[i] in Ergebnisse['Absorption']:
+        Ergebnisse['Absorption'][name[i]] = {}
+    Ergebnisse['Absorption'][name[i]]['theta_K'] = theta_K[i]
+    Ergebnisse['Absorption'][name[i]]['l_K'] = l_K[i]
+    Ergebnisse['Absorption'][name[i]]['E_K'] = E_K[i]
+    Ergebnisse['Absorption'][name[i]]['sigma_K'] = sigma_K[i]
+
+json.dump(Ergebnisse,open('data/Ergebnisse.json','w'),indent=4)
+
 
 ###############################
 ## Plots

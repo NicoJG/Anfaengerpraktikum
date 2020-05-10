e89c004be553578b5aeba718d5f31204d9c1e575
Nico Guth <nico.guth@tu-dortmund.de>
2020-05-10 17:26:11 +0200

    V602 Python Absorption Neu

--- V602_Nico/python/absorption.py
+++ V602_Nico/python/absorption.py
@@ -15,54 +15,29 @@ np.set_printoptions(threshold=sys.maxsize)
 # Matrizen initialisieren var[Element][0:Element_size]
 theta = np.zeros([6,21]) 
 N = np.zeros([6,21]) 
+name = ['Zink','Gallium','Brom','Rubidium','Strontium','Zirkonium']
+size = [16,21,16,14,16,16]
 
-##############################
 ## Messwerte einlesen
-##############################
-# Messwerte Zink einlesen
-Zn = 0
-Zn_size = 16
-theta[Zn][0:Zn_size],N[Zn][0:Zn_size] = np.genfromtxt('data/Zink.dat',delimiter=',',unpack=True)
-
-# Messwerte Gallium einlesen
-Ga = 1
-Ga_size = 21
-theta[Ga][0:Ga_size],N[Ga][0:Ga_size] = np.genfromtxt('data/Gallium.dat',delimiter=',',unpack=True)
-
-# Messwerte Brom einlesen
-Br = 2
-Br_size = 16
-theta[Br][0:Br_size],N[Br][0:Br_size] = np.genfromtxt('data/Brom.dat',delimiter=',',unpack=True)
-
-# Messwerte Rubidium einlesen
-Rb = 3
-Rb_size = 14
-theta[Rb][0:Rb_size],N[Rb][0:Rb_size] = np.genfromtxt('data/Rubidium.dat',delimiter=',',unpack=True)
-
-# Messwerte Strontium einlesen
-Sr = 4
-Sr_size = 16
-theta[Sr][0:Sr_size],N[Sr][0:Sr_size] = np.genfromtxt('data/Strontium.dat',delimiter=',',unpack=True)
-
-# Messwerte Zirkonium einlesen
-Zr = 5
-Zr_size = 16
-theta[Zr][0:Zr_size],N[Zr][0:Zr_size] = np.genfromtxt('data/Zirkonium.dat',delimiter=',',unpack=True)
+for i in range(0,6):
+    theta[i][0:size[i]],N[i][0:size[i]] = np.genfromtxt('data/'+name[i]+'.dat',delimiter=',',unpack=True)
 
 ###############################
 ## Plots
 ###############################
-##Zink
-# Plot der Messwerte
-plt.plot(theta[Zn][0:Zn_size],N[Zn][0:Zn_size], 'ro', label='Messwerte')
+for i in range(0,6):
+    print('Plot: '+name[i])
+    # Plot der Messwerte
+    plt.plot(theta[i][0:size[i]],N[i][0:size[i]], 'ro', label='Messwerte')
 
-# Achsenbeschriftung
-plt.xlabel(r'$\theta \:/\: \si{\degree}$')
-plt.ylabel(r'$N \:/\: \si{\frac{Impulse}{\second}}$')
+    # Achsenbeschriftung
+    plt.xlabel(r'$\theta \:/\: \si{\degree}$')
+    plt.ylabel(r'$N \:/\: \si{\frac{Impulse}{\second}}$')
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.legend()
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+    # in matplotlibrc leider (noch) nicht möglich
+    plt.legend()
+    plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 
-# Plot speichern
-plt.savefig('build/plot_zink.pdf')
+    # Plot speichern
+    plt.savefig('build/plot_'+name[i]+'.pdf')
+    plt.clf()

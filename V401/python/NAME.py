03c1fd10d453774ffd535d27976eef879b0b7fcd
Daverku <david.venker@tu-dortmund.de>
2020-07-19 17:56:34 +0200

    V401 Python halt

--- V401/python/NAME.py
+++ V401/python/NAME.py
@@ -9,12 +9,23 @@ import numpy as np
 import json
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
+from scipy import stats
 
 # Funktion für Curve Fit:
 def l(z):
     return 2*0.005/(z * 5.017)
 
+def n(z,p):
+    return 1 + (z*lam)/(2*b) * (T)/(T0) * (p0)/(p0 - p)
 # Daten einlesen
 z = np.genfromtxt('data/wegunterschied.csv',delimiter=',',unpack=True)
+p_minus, z_luft = np.genfromtxt('data/luft.csv',delimiter=',',unpack=True)
 
-print(l(z)*10**9)
+lam = 693.16 * 10**-9
+T0 = 273.15
+p0 = 1.0132
+b = 0.050
+T = 293.15
+p = 1 + p_minus
+
+#print(np.mean(n(z_luft, p)))
